67a84b99b01055a0517575eb5ce02a2ce897ae02
hubiss <hubiss.lab@gmail.com>
2017-02-10 19:29:11 +0900

    SI version 2.0 has released

--- si-onem2m-src/src/main/java/net/herit/iot/onem2m/bind/http/client/AsyncResponseListener.java
+++
@@ -1,7 +0,0 @@
-package net.herit.iot.onem2m.bind.http.client;
-
-import net.herit.iot.message.onem2m.OneM2mResponse;
-
-public interface AsyncResponseListener {
-	public void asyncResponse(OneM2mResponse resMessage);
-}
